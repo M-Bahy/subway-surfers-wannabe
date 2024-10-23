ee69922410ba7e4b29408b80df9a713f8b3f5cd1
M-Bahy <mohamedayman2942002@gmail.com>
2024-10-23 15:43:43 +0300

    game over screen

---
+++ Assets/scripts/Game Over Script.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScript : MonoBehaviour
+{
+
+
+    public TMP_Text scoreText; // Score : 0
+     public Button restartButton;
+
+    public Button mainMenuButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score : " + PlayerScript.score.ToString();
+        restartButton.onClick.AddListener(RestartGame);
+        mainMenuButton.onClick.AddListener(GoToMainMenu);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+       SceneManager.LoadScene("Title Screen"); 
+    }
+      public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("SampleScene"); 
+    }
+}
